e5508aae8167d2466e5b3fdb8c3ec5c97c0b601e
Graffini <35291741+Graffini@users.noreply.github.com>
2023-04-30 21:28:23 +0200

    add further translations

--- babele-register.js
+++ babele-register.js
@@ -1,8 +1,8 @@
 Hooks.once('init', () => { 
 	if(typeof Babele !== 'undefined') { 
 		Babele.get().register({
-			module: 'bid-pt-br',
-			lang: 'pt-BR',
+			module: 'bitd-foundry-de',
+			lang: 'de',
 			dir: 'compendium'
 		}); 
 	}
